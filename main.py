380a5361458f44f730ab51b1327c2ddc8e402813
Alexander Hecke <alexander.hecke@univie.ac.at>
2023-12-24 01:40:08 +0100

    impl mvc pattern

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import time
 from tkinter import *
 from tkinter import ttk, messagebox
 from tkinter.filedialog import askopenfilename
@@ -8,97 +9,143 @@ import whisper
 file_to_transcribe: str | None = None
 
 
-def get_file_pick():
-    ALLOWED_FILE_TYPES = [
-        ("MPEG file", ("*.mp3","*.mp4")),
-        # ("MP4 file", "*.mp4"),
-    ]
+class View:
+    def __init__(self, controller: "Controller"):
+        self.controller = controller
 
-    picked_file = askopenfilename(filetypes=ALLOWED_FILE_TYPES)
-    print(picked_file)
+        # init UI elements
+        self.root = Tk()
+        self.root_frame = ttk.Frame(self.root, padding=16)
+        self.control_frame = ttk.Frame(self.root_frame, padding=16)
+        self.selected_file_label = ttk.Label(self.control_frame, text="")
+        self.start_button = ttk.Button(
+            self.control_frame, text="Start", command=self.controller.init_transcription
+        )
 
-    return picked_file if picked_file != () else None
+        ttk.Label(
+            self.control_frame,
+            text="Pick a file to transcribe!\n"
+            "Program will freeze while transcription is in progress!",
+            font="bold",
+        ).grid(column=0, row=0)
+        ttk.Button(
+            self.control_frame, text="Pick file", command=self.controller.select_file
+        ).grid(column=0, row=2)
 
+        # set some UI params
+        self.root.minsize(500, 200)
+        self.root.title(string="Helga's tool")
+        self.root_frame.grid()
+        self.control_frame.grid(column=0, row=0)
+        self.selected_file_label.grid(column=0, row=1)
 
-def select_file():
-    global selected_file_label, file_to_transcribe
-    picked_file = get_file_pick()
+        self.start_button.grid(column=1, row=2)
+        self.start_button.config(state="disabled")
 
-    if picked_file is not None and os.path.exists(picked_file):
-        file_to_transcribe = picked_file
-        selected_file_label.configure(text=f"File selected: {picked_file}")
-        start_button.config(state="normal", text="Start")
+    def update(self, state: "State"):
+        if state.file_to_transcribe is not None:
+            self.start_button.config(state="normal", text="Start")
+            self.selected_file_label.config(
+                text=f"File picked: {state.file_to_transcribe}"
+            )
+        else:
+            self.start_button.config(state="disabled", text="Start")
+            self.selected_file_label.config(text="")
 
-
-def init_transcription():
-    global file_to_transcribe
-    start_button.config(state="disabled", text="Transcribing...")
-    print(f"Transcribing {file_to_transcribe}.")
-
-    try:
-        result_text = transcribe(file_to_transcribe)
-        write_transcription_to_disk(result_text, file_to_transcribe)
-    except Exception as ex:
-        messagebox.showerror("Error", str(ex))
-    finally:
-        start_button.config(state="disabled", text="Start")
-        file_to_transcribe = None
-        selected_file_label.config(text="")
-        messagebox.showinfo("Success", "File successfully transcribed!")
-
-
-def transcribe(file):
-    if not os.path.exists(file):
-        raise ValueError(f"File does not exist ({file}).")
-
-    model = whisper.load_model("small")
-    result = model.transcribe(file, language="de")
-
-    # gotta pull out the segments one at a time.
-    segments = list(map(lambda s: s["text"], result["segments"]))
-    return os.linesep.join(segments)
+    def show_transcription_start(self):
+        self.start_button.config(state="disabled", text="Transcribing...")
 
 
-def write_transcription_to_disk(result, transcribed_file: str | None):
-    if transcribed_file is None:
-        raise ValueError("Transcribed file cannot be None.")
+class State:
+    def __init__(self):
+        self.__file_to_transcribe: str | None = None
 
-    # clip off file extension
-    filename_no_extension = ".".join(transcribed_file.split(".")[:-1])
+        self.__views: list[View] = []
 
-    result_filename = ".".join((filename_no_extension, "txt"))
+    def update_views(self):
+        for view in self.__views:
+            view.update(self)
 
-    with open(result_filename, "w") as file:
-        file.write(result)
+    def add_view(self, new_view: View):
+        if new_view not in self.__views:
+            self.__views.append(new_view)
+
+    def rm_view(self, view_to_rm: View):
+        if view_to_rm in self.__views:
+            self.__views.remove(view_to_rm)
+
+    def update_file(self, new_file_to_transcribe):
+        self.__file_to_transcribe = new_file_to_transcribe
+        self.update_views()
+
+    def signal_transcription_start(self):
+        for view in self.__views:
+            view.show_transcription_start()
+
+    @property
+    def file_to_transcribe(self):
+        return self.__file_to_transcribe
 
 
-root = Tk()
-root.minsize(500, 200)
-root.title(string="Helga's tool")
+class Controller:
+    def __init__(self, state: State):
+        self.__state = state
 
-# frame hierarchy
-root_frame = ttk.Frame(root, padding=16)
-root_frame.grid()
+    def select_file(self):
+        picked_file = askopenfilename(filetypes=[("MPEG file", ("*.mp3", "*.mp4"))])
+        if picked_file is not None and os.path.exists(picked_file):
+            self.__state.update_file(picked_file)
 
-control_frame = ttk.Frame(root_frame, padding=16)
-control_frame.grid(column=0, row=0)
+    @staticmethod
+    def __transcribe(file):
+        if not os.path.exists(file):
+            raise ValueError(f"File does not exist ({file}).")
 
-# control frame: buttons and list which file is selected
-ttk.Label(
-    control_frame,
-    text="Pick a file to transcribe!\n"
-    "Program will freeze while transcription is in progress!",
-    font="bold",
-).grid(column=0, row=0)
-selected_file_label = ttk.Label(control_frame, text="")
-selected_file_label.grid(column=0, row=1)
+        model = whisper.load_model("small")
+        result = model.transcribe(file, language="de")
 
-ttk.Button(control_frame, text="Pick file", command=select_file).grid(column=0, row=2)
+        # gotta pull out the segments one at a time.
+        segments = list(map(lambda s: s["text"], result["segments"]))
+        return os.linesep.join(segments)
 
-start_button = ttk.Button(control_frame, text="Start", command=init_transcription)
-start_button.grid(column=1, row=2)
-start_button.config(state="disabled")
+    @staticmethod
+    def __write_transcription_to_disk(result, transcribed_file):
+        if transcribed_file is None:
+            raise ValueError("Transcribed file cannot be None.")
+
+        # clip off file extension
+        filename_no_extension = ".".join(transcribed_file.split(".")[:-1])
+
+        result_filename = ".".join((filename_no_extension, "txt"))
+
+        with open(result_filename, "w") as file:
+            file.write(result)
+
+    def init_transcription(self):
+        self.__state.signal_transcription_start()
+        print(f"Transcribing {self.__state.file_to_transcribe}.")
+        time.sleep(1.5)
+
+        try:
+            result_text = self.__transcribe(self.__state.file_to_transcribe)
+            self.__write_transcription_to_disk(
+                result_text, self.__state.file_to_transcribe
+            )
+            messagebox.showinfo("Success", "File successfully transcribed!")
+        except Exception as ex:
+            messagebox.showerror("Error", str(ex))
+        finally:
+            self.__state.update_file(None)
 
 
 if __name__ == "__main__":
-    root.mainloop()
+    # root.mainloop()
+
+    # init MVC pattern
+    state = State()
+    controller = Controller(state)
+    view = View(controller)
+    state.add_view(view)
+
+    # and run program
+    view.root.mainloop()
